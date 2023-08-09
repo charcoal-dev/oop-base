54f90ff9f500e761c63e92a949b5ac6e4dbbff04
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2023-08-09 21:33:48 +0400

    case styles convention

---
+++ src/CaseStyles.php
@@ -0,0 +1,94 @@
+<?php
+/*
+ * This file is a part of "charcoal-dev/oop-base" package.
+ * https://github.com/charcoal-dev/oop-base
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/charcoal-dev/oop-base/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\OOP;
+
+/**
+ * Class CaseStyles
+ * @package Charcoal\OOP
+ */
+class CaseStyles
+{
+    /**
+     * Converts given string (i.e. snake_case) to PascalCase (aka UpperCamelCase)
+     * @param string $name
+     * @param bool $cleanStr
+     * @return string
+     */
+    public static function PascalCase(string $name, bool $cleanStr = true): string
+    {
+        if (!$name) {
+            return "";
+        }
+
+        $words = preg_split("/[^a-zA-Z0-9]+/", static::snake_case($name, $cleanStr), 0, PREG_SPLIT_NO_EMPTY);
+        return implode("", array_map(function ($word) {
+            return ucfirst($word);
+        }, $words));
+    }
+
+    /**
+     * Converts given string (i.e. snake_case) to camelCase
+     * @param string $name
+     * @param bool $cleanStr
+     * @return string
+     */
+    public static function camelCase(string $name, bool $cleanStr = true): string
+    {
+        if (!$name) {
+            return "";
+        }
+
+        return lcfirst(self::PascalCase($name, $cleanStr));
+    }
+
+    /**
+     * Converts given string (i.e. PascalCase or camelCase) to snake_case
+     * @param string $name
+     * @param bool $cleanStr
+     * @return string
+     */
+    public static function snake_case(string $name, bool $cleanStr = true): string
+    {
+        if (!$name) {
+            return "";
+        }
+
+        if ($cleanStr) {
+            $name = preg_replace('/\W/', '', $name);
+        }
+
+        // Convert PascalCase word to camelCase
+        $name = sprintf("%s%s", strtolower($name[0]), substr($name, 1));
+
+        // Split words
+        $words = preg_split("/([A-Z0-9]+)/", $name, 0, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
+        $wordsCount = count($words);
+        $snake = $words[0];
+
+        // Iterate through words
+        for ($i = 1; $i < $wordsCount; $i++) {
+            if ($i % 2 != 0) {
+                // Add an underscore on an odd $i
+                $snake .= "_";
+            }
+
+            // Add word to snake
+            $snake .= $words[$i];
+        }
+
+        // Return lowercase snake
+        return strtolower($snake);
+    }
+}
---
+++ tests/CaseStylesTest.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * This file is a part of "charcoal-dev/oop-base" package.
+ * https://github.com/charcoal-dev/oop-base
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/charcoal-dev/oop-base/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+/**
+ * Class CaseStylesTest
+ */
+class CaseStylesTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @return void
+     */
+    public function test_caseConversions(): void
+    {
+        // Mix-cases
+        $this->assertEquals("charcoal_dev", \Charcoal\OOP\CaseStyles::snake_case("charcoalDev"));
+        $this->assertEquals("CharcoalDev", \Charcoal\OOP\CaseStyles::PascalCase("charcoal_dev"));
+        $this->assertEquals("charcoalDev", \Charcoal\OOP\CaseStyles::camelCase("charcoal_dev"));
+        $this->assertEquals("charcoal_dev_test", \Charcoal\OOP\CaseStyles::snake_case("CharcoalDevTest"));
+        $this->assertEquals("CharcoalDevTest", \Charcoal\OOP\CaseStyles::PascalCase("charcoal_dev_test"));
+        $this->assertEquals("charcoalDevTest", \Charcoal\OOP\CaseStyles::camelCase("charcoal_dev_test"));
+
+        // snake_case
+        $this->assertEquals("charcol_dev", \Charcoal\OOP\CaseStyles::snake_case("charco@l_dev"));
+        $this->assertEquals("charcoal_dev", \Charcoal\OOP\CaseStyles::snake_case("CharcoalDev"));
+        $this->assertEquals("charcoal_dev", \Charcoal\OOP\CaseStyles::snake_case("charcoalDev "));
+        $this->assertEquals("charcoaldev", \Charcoal\OOP\CaseStyles::snake_case("Charcoaldev"));
+
+        // PascalCase
+        $this->assertEquals("CharcoalDev", \Charcoal\OOP\CaseStyles::PascalCase("CharcoalDev"));
+        $this->assertEquals("CharcoalDev", \Charcoal\OOP\CaseStyles::PascalCase("charcoal_dev"));
+        $this->assertEquals("CharcoalDev", \Charcoal\OOP\CaseStyles::PascalCase("charcoalDev"));
+        $this->assertEquals("Charcoaldev", \Charcoal\OOP\CaseStyles::PascalCase("charcoaldev"));
+
+        // camelCase
+        $this->assertEquals("charcoalDev", \Charcoal\OOP\CaseStyles::camelCase("charcoal_dev"));
+        $this->assertEquals("charcoalDev", \Charcoal\OOP\CaseStyles::camelCase("CharcoalDev"));
+        $this->assertEquals("charcoalDev", \Charcoal\OOP\CaseStyles::camelCase("charcoalDev"));
+    }
+}
